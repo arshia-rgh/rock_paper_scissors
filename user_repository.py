977d5aaf3fd072099cd8ffaeb1a4686f50e6bd84
arshia <arshiarezagholi1212@gmail.com>
2024-08-05 17:44:12 +0330

    added user repository

--- user_repository.py
+++ user_repository.py
@@ -1,25 +1,53 @@
-from typing import Type
+from typing import Type, List
 
 from sqlalchemy.orm import Session
 from database.models import User
 from database.base import session
+from utils.exception import UserNotExists
 
 
 class UserRepository:
-    def __init__(self, db: Session):
+    def __init__(self, db: Session) -> None:
         self.db = db
 
-    def get_all(self):
-        ...
+    def get_all(self) -> list[Type[User]]:
+        return self.db.query(User).all()
 
-    def get_by_name(self):
-        ...
+    def get_by_name(self, name: str) -> User | None:
+        user = self.db.query(User).filter(User.name == name).first()
+        return user if user else None
 
-    def create(self):
-        ...
+    def create(self, item: User) -> User | Exception:
+        try:
+            user = item
+            self.db.add(item)
+            self.db.commit(item)
+            self.db.refresh(item)
+            return user
+        except Exception as e:
+            return e
 
-    def delete(self):
-        ...
+    def delete(self, id: int) -> bool:
+        user = self.db.query(User).filter(User.id == id).first()
+        if user:
+            self.db.delete(user)
+            self.db.commit()
+            return True
+        else:
+            raise UserNotExists
 
-    def update(self):
-        ...
+    def update(self, item: User) -> User | Exception:
+        user = self.db.query(User).filter(User.id == item.id).first()
+        if user:
+            try:
+                user = item
+                self.db.commit()
+                self.db.refresh(item)
+                return user
+            except Exception as e:
+                return e
+        else:
+            raise UserNotExists
+
+
+user_repo = UserRepository(session)
---
+++ utils/exception.py
@@ -0,0 +1,2 @@
+class UserNotExists(BaseException):
+    pass
