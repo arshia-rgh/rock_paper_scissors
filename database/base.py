5b90524997c4e9da3ced56cdd74dfdefe527e5d9
arshia <arshiarezagholi1212@gmail.com>
2024-08-05 17:03:41 +0330

    basic configs for sqlalchemy

---
+++ database/base.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from config import Config
+
+DB_URL = f"mysql+mysqlconnector://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}"
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+session = SessionLocal()
+base = declarative_base()
