6a2d8f21334b556745c3a6dee24eaffd6974068e
arshia <arshiarezagholi1212@gmail.com>
2024-08-07 13:16:34 +0330

    bug fixed

--- client/client_socket.py
+++ client/client_socket.py
@@ -3,3 +3,5 @@ import socket
 from config import SocketConfig
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((SocketConfig.HOST_IP, SocketConfig.HOST_PORT))
