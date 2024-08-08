f92ba0643524d34023101faabd042968fc61bbda
arshia <arshiarezagholi1212@gmail.com>
2024-08-08 23:23:41 +0330

    removed start_game menu and added this option to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+echo "Select game mode : (1) Solo (2) Multiplayer"
+read -r game_mode
+
+
 # Start the server in the background
-python3 server.py &
+python3 server.py "$game_mode" &
 
 # Wait for a few seconds to ensure the server is up and running
 sleep 2
--- server.py
+++ server.py
@@ -3,32 +3,33 @@ import threading
 
 import typer
 
-from settings.config import SocketConfig
-from menus.constants import *
 from main_solo import *
-
+from menus.constants import *
+from menus.menu import Menu
+from settings.config import SocketConfig
+import sys
 clients = []
 
 
-def handle_client(client_socket):
+def handle_client(client_socket, game_mode):
     clients.append(client_socket)
+    menu = Menu(client_socket)
     while True:
         try:
-            selected = main_menu(client_socket)
+            selected = menu.main_menu()
             if selected == MainMenuOption.START.value:
-                selected_mode = start_game_menu(client_socket)
-                if selected_mode == StartMenuOption.AI.value:
+                if game_mode == "1":
                     play__game_ai(client_socket)
-                elif selected_mode == StartMenuOption.PLAYER.value:
+                elif game_mode == "2":
                     if len(clients) < 2:
                         client_socket.send("Waiting for another player to join...\n".encode())
                         while len(clients) < 2:
                             pass
                     play__game_player(client_socket, clients)
             elif selected == MainMenuOption.CREATE.value:
-                create_player_menu(client_socket)
+                menu.create_player_menu()
             elif selected == MainMenuOption.LEADERBOARD.value:
-                leaderboard_menu(client_socket)
+                menu.leaderboard_menu()
             else:
                 client_socket.send("Goodbye :red_heart-emoji:".encode())
                 break
@@ -38,7 +39,7 @@ def handle_client(client_socket):
     client_socket.close()
 
 
-def main():
+def main(game_mode: str):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_socket.bind((SocketConfig.HOST_IP, SocketConfig.HOST_PORT))
@@ -47,9 +48,10 @@ def main():
     while True:
         client_socket, addr = server_socket.accept()
         print(f"Connection from {addr} has been established.")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, game_mode))
         client_handler.start()
 
 
 if __name__ == "__main__":
+    game_mode = sys.argv[1] if len(sys.argv) > 1 else "1"
     typer.run(main)
